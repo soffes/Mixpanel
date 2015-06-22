4406a0b53b7f240d703ad2b6c2a1c6819e81974d
Sam Soffes <sam@soff.es>
2015-06-21 23:18:21 -0700

    Setup tests and watchOS target

--- Mixpanel/Mixpanel.h
+++ Mixpanel/Mixpanel.h
@@ -3,7 +3,7 @@
 //  Mixpanel
 //
 //  Created by Sam Soffes on 4/28/15.
-//  Copyright (c) 2015 Sam Soffes. All rights reserved.
+//  Copyright © 2015 Sam Soffes. All rights reserved.
 //
 
 @import Foundation;
---
+++ Mixpanel/Tests/MixpanelTests.swift
@@ -0,0 +1,14 @@
+//
+//  MixpanelTests.swift
+//  MixpanelTests
+//
+//  Created by Sam Soffes on 6/21/15.
+//  Copyright © 2015 Sam Soffes. All rights reserved.
+//
+
+import XCTest
+import Mixpanel
+import DVR
+
+class MixpanelTests: XCTestCase {
+}
